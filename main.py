19c4f9babc86887d08deae63ee9cb9381175e5d8
Sibints2000 <sibints2000@gmail.com>
2021-08-15 09:47:49 +0530

    Sixth Commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import another_module
-print(another_module.another_variable)
+# import another_module
+# print(another_module.another_variable)
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
